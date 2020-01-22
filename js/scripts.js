15d7d74175975c8841f42cf70587a4b107ff80e8
Steven Fleming and Tiffany Siu <tiffany@>
2020-01-22 13:42:26 -0800

    add output calculation and results

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,40 @@
 $(document).ready(function(){
   $("#form1").submit(function(event){
+    var warn = 1;
+    var symp = 2;
+    var cop = -1;
+    var total = 0;
+    var high = 7;
+    var low = 0;
+
+    $("input:checkbox[name=warningSigns]:checked").each(function(){
+      total += warn;
+    });
+  
+    $("input:checkbox[name=symptoms]:checked").each(function() {
+      total += symp;
+    });
+
+
+    $("input:checkbox[name=coping]:checked").each(function() {
+      total += cop;
+    });
+
+    $("#results").show();
+    if(total > high){
+      $("#serious").show();
+      $("#healthy").hide();
+      $("#moderate").hide();
+    }else if(total <= low){
+      $("#healthy").show();
+      $("#moderate").hide();
+      $("#serious").hide();
+    }else{
+      $("#moderate").show();
+      $("#serious").hide();
+      $("#healthy").hide();
+    }
+
     event.preventDefault();
   });
 });
